4688085e316da37e952303524a32a0962806defc
Matheus <matheuszoiim@gmail.com>
2021-03-22 01:25:02 -0300

    primeiro commit

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'perfis',
     'usuarios',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- perfis/views.py
+++ perfis/views.py
@@ -3,6 +3,90 @@ from django.http import HttpResponse
 from perfis.models import Perfil, Convite
 from django.contrib.auth.decorators import login_required, permission_required
 
+
+from rest_framework import viewsets, response, status, exceptions
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.permissions import AllowAny
+
+
+
+
+class PerfilViewSet(viewsets.ModelViewSet):
+    queryset = Perfil.objects.all()
+    serializer_class = None
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return None
+
+        return super().get_serializer_class()
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return (AllowAny(),)
+
+        return super().get_permissions()
+
+
+@api_view(['GET'])
+@renderer_classes((JSONRenderer, BrowsableAPIRenderer))
+def get_convites(request, *args, **kwargs):
+
+    perfil_logado = get_perfil_logado(request)
+    convites = Convite.objects.filter(convidado=perfil_logado)
+    serializer = None
+
+    return response.Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@renderer_classes((JSONRenderer, BrowsableAPIRenderer))
+def convidar(request, *args, **kwargs):
+
+    try:
+        perfil_a_convidar = Perfil.objects.get(id=kwargs['perfil_id'])
+    except:
+        raise exceptions.NotFound('Não foi encontrado um perfil com o id informado.')
+    
+    perfil_logado = get_perfil_logado(request)
+
+    if perfil_a_convidar != perfil_logado:
+        perfil_logado.convidar(perfil_a_convidar)
+        return response.Response(
+            'mensagem' : f'Convite enviado com sucesso para {perfil_a_convidar.email}'),
+            status=status.HTTP_201_CREATED
+    
+    raise exceptions.ParseError('Você não pode convidar o perfil com o id informado.')
+
+
+@api_view(['POST'])
+@renderer_classes((JSONRenderer, BrowsableAPIRenderer))
+def aceitar(request, *args, **kwargs):
+
+    perfil_logado = get_perfil_logado(request)
+
+    try:
+        convite = Convite.objects.filter(convidado=perfil_logado).get(id=kwargs['convite_id'])
+    except:
+        raise exceptions.NotFound(f'Não foi encontrado um convite com o id {kwargs['convite_id']} informado.')
+    
+    convite.aceitar()
+    return response.Responde({'mensagem': 'Convite aceito com sucesso'},
+                            status = status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+@renderer_classes((JSONRenderer, BrowsableAPIRenderer))
+def get_meu_perfil(request, *args, **kwargs):
+
+    perfil_logado = get_perfil_logado(request)
+    serializer = None
+    return response.Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
 @login_required
 def index(request):
     print(request.user.username)
@@ -26,7 +110,7 @@ def convidar(request, perfil_id):
     perfil_logado.convidar(perfil_a_convidar)
     return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
 
-@login_required
+
 def get_perfil_logado(request):
     return request.user.perfil
 
